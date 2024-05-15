01135420c973bf01c405e5bbea3cc92f36afbdaa
marco-29 <126988064+marco-29@users.noreply.github.com>
2024-05-15 16:06:02 -0600

    0.0.29 Se agrego sub-menu a servicios y vistas

--- menu.php
+++ menu.php
@@ -15,11 +15,20 @@
                             <!-- logo close -->
                         </div>
                     </div>
+
+
                     <div class="de-flex-col header-col-mid">
                         <ul id="mainmenu">
                             <li><a class="menu-item" href="index.php">Inicio</a></li>
                             <li><a class="menu-item" href="nosotros.php">Nosotros</a></li>
-                            <li><a class="menu-item" href="servicios.php">Servicios</a></li>
+                            <li><a class="menu-item" href="servicios.php">Servicios</a>
+                                <ul class="sub-menu">
+                                    <li><a href="poliza_juridica.php">Póliza Jurídica</a></li>
+                                    <li><a href="convenio_prevencion.php">Convenio de Prevención de Conflictos</a></li>
+                                    <li><a href="convenio_transaccion.php">Convenio de Transacción</a></li>
+                                    <li><a href="investigacios_inquilinos.php">Investigación de Inquilinos</a></li>
+                                </ul>
+                            </li>
                             <li><a class="menu-item" href="sucursales.php">Sucursales</a></li>
                             <li><a class="menu-item" href="blog.php">Blog</a></li>
                             <li><a class="menu-item" href="contacto.php">Contacto</a></li>
